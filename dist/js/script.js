127288241aff3907d1ade71db277e0e14b8f00e4
codenamedcd <codenamedcd@gmail.com>
2023-08-18 22:19:18 +0700

    first commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,32 @@
 //burger
-const burger = document.querySelector('#burger');
-const navMenu = document.querySelector('#nav-menu');
+const burger = document.querySelector("#burger");
+const navMenu = document.querySelector("#nav-menu");
 
-burger.addEventListener('click', function() {
-    burger.classList.toggle('burger-active');
-    navMenu.classList.toggle('hidden');
+burger.addEventListener("click", function () {
+  burger.classList.toggle("burger-active");
+  navMenu.classList.toggle("nav-active");
 });
 
 //navar fixed
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const fixNav = header.offsetTop;
+  let sections = document.querySelectorAll("section");
 
-    if (window.pageYOffset > fixNav) {
-        header.classList.add('navbar-fixed');
+  if (window.pageYOffset > fixNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("animate-show");
     } else {
-        header.classList.remove('navbar-fixed');
+      sec.classList.remove("animate-show");
     }
-}
-
-//title
-function titleshow(x) {
-  x.classList.toggle('sub-change');
-}
+  });
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html", "tentang.html", "kontak.html"],
   theme: {
     extend: {
       container: {
-        center:true,
-        padding: '16px',
+        center: true,
+        padding: "16px",
       },
-      screens:{
-          '2xl':'1320px',
-      },
-      scale: {
-        '2.1': '2.1',
+      screens: {
+        "2xl": "1320px",
       },
       colors: {
-        dark: '#1e1c1c',
-        milk: '#e5e5e5',
-        primary : '#3DB1AA',
-        secprimary: '#262626',
+        dark: "#1e1c1c",
+        milk: "#e5e5e5",
+        primary: "#F1B424",
+        secprimary: "#D19003",
       },
     },
   },
   plugins: [],
-}
-
+};
