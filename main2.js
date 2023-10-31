a8f4e2c2842bb06acef124302ac96b9cb22c0824
codenamedcd <130311327+codenamedcd@users.noreply.github.com>
2023-10-31 22:29:25 +0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -2,7 +2,7 @@ const projects = [
   {
     judul: "Word Counter",
     subjudul: "Penghitung jumlah kata dan huruf serta pengubah kapital",
-    link: "https://codenamedcd.github.io/wordcounter/",
+    link: "https://wordcounter-dcd.vercel.app/",
   },
 ];
 
